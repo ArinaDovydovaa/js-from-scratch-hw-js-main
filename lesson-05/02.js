d8be0767419b960e22d76e55817ac385de32d832
Arina Dovydovaa <arina.dovydova95@gmail.com>
2026-06-21 19:24:43 +0300

    lesson-05/02

--- lesson-05/02.js
+++ lesson-05/02.js
@@ -35,4 +35,13 @@ const gallery = {
   'The Scream': 'Edvard Munch',
 }
 
-function updateGallery() {}
+function updateGallery(objectGallery, name, newValue) {
+  if (gallery.hasOwnProperty(name)) {
+    gallery[name] = newValue;
+  } else {gallery[name] = newValue;}
+}
+
+updateGallery(gallery, 'Mona Lisa', 'Leonardo da Vinci, 1503-1506')
+updateGallery(gallery, 'The Persistence of Memory', 'Salvador Dali')
+
+console.log(gallery)
