828eea1ea3b3f316ea81e9111a46d3b87d9a2cd3
Arina Dovydovaa <arina.dovydova95@gmail.com>
2026-07-10 21:42:19 +0300

    lesson-08/01

--- lesson-08/01.js
+++ lesson-08/01.js
@@ -6,4 +6,33 @@
   Другой вариант - использовать метод split строк, и метод массива join.
 */
 
-function capitalizeWords() {}
+function capitalizeWords(str){
+  if (!str || typeof str !== 'string') return str;
+
+  let result = '';
+  let capitalizeNext = true;
+
+  for (let i = 0; i < str.length; i++) {
+    const char = str[i];
+
+    if (char === ' ') {
+      result += char;
+      capitalizeNext = true;
+    } else if (capitalizeNext) {
+      result += char.toUpperCase();
+      capitalizeNext = false;
+    } else {
+      result += char.toLowerCase();
+    }
+  }
+
+  return result;
+}
+
+console.log(capitalizeWords("hello world from javascript"))
+
+// вар2
+// function capitalizeWords(str) {
+//   return str.split(' ').map(word => word.charAt(0).toUpperCase() + word.slice(1)).join(' ')
+// }
+// console.log(capitalizeWords("hello world from javascript"))
