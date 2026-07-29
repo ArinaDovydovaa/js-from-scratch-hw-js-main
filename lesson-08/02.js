41aa86a2e5e0e06ee01d17f6370e6a55f7de3813
Arina Dovydovaa <arina.dovydova95@gmail.com>
2026-07-29 15:17:25 +0300

    lesson-08/02

--- lesson-08/02.js
+++ lesson-08/02.js
@@ -4,11 +4,19 @@
  */
 
 function isNumeric(str) {
-  // your code
+  if (typeof str !== 'string') {
+    return false;
+  }
+  const trimmedStr = str.trim();
+
+  if (trimmedStr === '') {
+    return false;
+  }
+  return !isNaN(trimmedStr) && isFinite(trimmedStr) && trimmedStr !== '';
 }
 
-// console.log(isNumeric("123")) // Ожидаемый результат: true
-// console.log(isNumeric("12.3")) // Ожидаемый результат: true
-// console.log(isNumeric("123abc")) // Ожидаемый результат: false
-// console.log(isNumeric("abc")) // Ожидаемый результат: false
-// console.log(isNumeric(" ")) // Ожидаемый результат: false
+console.log(isNumeric("123")) // Ожидаемый результат: true
+console.log(isNumeric("12.3")) // Ожидаемый результат: true
+console.log(isNumeric("123abc")) // Ожидаемый результат: false
+console.log(isNumeric("abc")) // Ожидаемый результат: false
+console.log(isNumeric(" ")) // Ожидаемый результат: false
